5ee430daccb868a21b4a1ea3b0825f2834d9d9c2
skyminds <skyminds@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-08 09:18:17 +0000

    Fix for the login loop that occured when using the password recovery and when logging straight from the login page.
    git-svn-id: https://plugins.svn.wordpress.org/sky-login-redirect/trunk@622497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sky-login-redirect.php
+++ sky-login-redirect.php
@@ -3,25 +3,25 @@
 Plugin Name: Sky Login Redirect
 Plugin URI: http://www.skyminds.net/wordpress-plugins/sky-login-redirect/
 Description: Redirects users to the page they were reading just before logging in.
-Version: 1.1
+Version: 1.2
 Author: Matt
 Author URI: http://www.skyminds.net/
 License: GPLv2 or later
 */
 function sky_login_redirect() {
-	$sky_referer  = $_SERVER['HTTP_REFERER'];
 	$sky_site_url = home_url();
+	$sky_referer  = $_SERVER['HTTP_REFERER'];
 	$redirect_to  = $_REQUEST['redirect_to'];
 
-	/* check if $redirect_to is set, not empty and belongs to our domain  */
-	if( isset($redirect_to) && !empty($redirect_to) && strpos($redirect_to, $_SERVER['HTTP_HOST']) )
-	{
+	/* check if $redirect_to is set, not empty and belongs to our home URL */
+	if( isset( $redirect_to ) && !empty( $redirect_to ) && strpos( $redirect_to, $sky_site_url ) ) {
+		/* yes it is, use $redirect_to */
 		return $redirect_to;
 	}
 
-	/* else check if referer belongs to our domain */
-	elseif( strpos($sky_referer, $_SERVER['HTTP_HOST']) ){
-		/* Smooth transparent redirects to the previous page */
+	/* else check if referer belongs to our home URL */
+	elseif( strpos( $sky_referer, $sky_site_url ) ){
+		/* Smooth transparent redirects to the referring page */
 		return $sky_referer;
 	}
 
@@ -30,5 +30,25 @@ function sky_login_redirect() {
 		return $sky_site_url;
 	}
 }
+
+function sky_login_url( $force_reauth, $redirect ) {
+	
+	/* define our login URL */	
+	$login_url = home_url('/wp-login.php');
+	
+	/* if $redirect is set, append it as argument to the login URL */
+	if ( !empty( $redirect ) ) {
+		$login_url = add_query_arg( 'redirect_to', urlencode( $redirect ), $login_url );
+	}
+	
+	/* if reauth is set, append it to the login URL as well */
+	if ( $force_reauth ) {
+		$login_url = add_query_arg( 'reauth', '1', $login_url ) ;
+	}
+
+	return $login_url ;
+}
+
 add_filter('login_redirect', 'sky_login_redirect');
+add_filter('login_url', 'sky_login_url', 10, 2);
 ?>
